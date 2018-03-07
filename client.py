53b4a613f0d36363454bf9bdfcbf484011ab4700
VCM admin account <vcm@vcm-3569.vm.duke.edu>
2018-03-07 15:37:29 -0500

    fix issues with client.py

--- client.py
+++ client.py
@@ -1,8 +1,19 @@
 import requests
-r = requests.get("http://vcm-3569.vm.duke.edu:5000/name")
-print(r)
-r2 = requests.get("http://vcm-3569.vm.duke.edu:5000/hello/Pamla Chace Gore")
-print(r2)
-r3 = requests.post("http://vcm-3569.vm.duke.edu:5000/distance", json={"a": [3,7], "b": [3,9]})
-distance_result = r3.json()
-print(distance_result)
+
+def getName():
+    r = requests.get("http://vcm-3569.vm.duke.edu:5000/name")
+    r_result = r.json()
+    print(r_result)
+    
+def getHelloMessage():
+    r2 = requests.get("http://vcm-3569.vm.duke.edu:5000/hello/Pamla Chace Gore")
+    r2_result = r2.json()
+    print(r2_result)
+
+def getDistance():
+    r3 = requests.post("http://vcm-3569.vm.duke.edu:5000/distance", json={"a": [3,7], "b": [3,9]})
+    distance_result = r3.json()
+    print(distance_result)
+
+if __name__ == '__main__':
+    main()
